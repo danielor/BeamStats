7ad99f83ce188f6f5cf9653a30e63685f366b3f1
Daniel Ortiz <danielor@umich.edu>
2021-03-07 15:50:22 -0800

    An Apache beam transform that computes the variance of a PCollection

---
+++ src/test/java/com/danielor/beamstats/VarianceTest.java
@@ -0,0 +1,44 @@
+package com.danielor.beamstats;
+
+import static org.apache.beam.sdk.testing.CombineFnTester.testCombineFn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.testing.CoderProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.danielor.beamstats.Variance.VarianceAccumulator;
+import com.danielor.beamstats.Variance.VarianceCoder;
+
+@RunWith(JUnit4.class)
+public class VarianceTest {
+
+  private static final Coder<VarianceAccumulator<Number>> CODER = new VarianceCoder<>();
+  private static final List<VarianceAccumulator<Number>> VALUES = 
+      Arrays.asList(new VarianceAccumulator<>(1, 3.0, 4.0),
+                    new VarianceAccumulator<>(2, 4.0, 5.0),
+                    new VarianceAccumulator<>(3, 5.0, 6.0));
+  
+  @Test
+  public void testVarianceAccumulatorEncodeDecode() throws Exception {
+    for (VarianceAccumulator<Number> value: VALUES) {
+      CoderProperties.coderDecodeEncodeEqual(CODER, value);
+    }
+  }
+  
+  @Test
+  public void testVarianceAccumulatorSerializable() throws Exception {
+    CoderProperties.coderSerializable(CODER);
+  }
+  
+  @Test
+  public void testVariance() throws Exception {
+    ArrayList<Number> list = new ArrayList<Number>(Arrays.asList(1, 2, 3, 4, 5));
+    testCombineFn(Variance.of(), list, 2.0);
+  }
+}
